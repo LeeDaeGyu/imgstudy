be2cdab7fd46349dd759291691bda86539d47a4f
doraemi <-->
2021-09-29 15:26:26 +0900

    Manage image file at front end from Using usestate

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("이미지를 선택해주세요.");
+  const formChange = e => {
+    const UploadedFile = e.target.files[0];
+    setFile(UploadedFile);
+    setFileName(UploadedFile.name);
+  };
   return (
     <form>
-      <label htmlFor="image">사진</label>
-      <input id="image" type="file" />
+      <label htmlFor="image">{fileName}</label>
+      <input id="image" type="file" onChange={formChange} />
       <button type="submit">제출</button>
     </form>
   );
