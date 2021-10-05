8eacd10ba83ddacaaa906aff019a4362105ab04c
doraemi <-->
2021-10-05 15:16:05 +0900

    Add image-preview system

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -9,10 +9,17 @@ const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState(defaultFileName);
   const [percent, setPercent] = useState(0);
+  const [imgSrc, setImgSrc] = useState(null);
+
   const imgSelectHandler = e => {
     const UploadedFile = e.target.files[0];
     setFile(UploadedFile);
     setFileName(UploadedFile.name);
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(UploadedFile);
+    fileReader.onload = e => {
+      setImgSrc(e.target.result);
+    };
   };
   const onSubmit = async e => {
     e.preventDefault();
@@ -30,19 +37,31 @@ const UploadForm = () => {
       setTimeout(() => {
         setPercent(0);
         setFileName(defaultFileName);
+        setImgSrc(null);
       }, 3000);
     } catch (err) {
       toast.warn("fail");
       console.error(err);
+      setFileName(defaultFileName);
       setPercent(0);
+      setImgSrc(null);
     }
   };
   return (
     <form onSubmit={onSubmit}>
+      <img
+        src={imgSrc}
+        className={`"image-preview" ${imgSrc && "image-preview-show"}`}
+      />
       <ProgressBar percent={percent} />
       <div className="file-dropper">
         {fileName}
-        <input id="image" type="file" onChange={imgSelectHandler} />
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          onChange={imgSelectHandler}
+        />
       </div>
       <button className="submitBtn " type="submit">
         제출
